af3a9abb2dab08ea687add9147c41d6756c2eec9
sadiahossain1 <sadiah16@nycstudents.net>
2023-10-20 12:24:05 -0400

    equation method left to complete

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class LinearEquation {
     /* Instance Variables */
     private int x1;
@@ -18,47 +20,29 @@ public class LinearEquation {
    the nearest hundredth */
     public double distance() {
         double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        distance = roundedToHundredth(distance);
         return distance;
     }
 
     /* Calculates and returns the y-intercept of the line between (x1, y1) and
        (x2, y2), rounded to the nearest hundredth */
     public double yIntercept() {
-        double slope = (y2 - y1) / (x2 - x1);
-        double yInt = y1 - (slope * x1);
+        double slope = (double) (y2 - y1) / (x2 - x1);
+        double yInt = (double) y1 - (slope * x1);
+        yInt = roundedToHundredth(yInt);
         return yInt;
     }
 
     /* Calculates and returns the slope of the line between (x1, y1) and
        (x2, y2), rounded to the nearest hundredth */
     public double slope() {
-        double slope = (y2 - y1) / (x2 - x1);
+        double slope = (double) (y2 - y1) / (x2 - x1);
+        slope = roundedToHundredth(slope);
         return slope;
     }
 
     /* Returns a String that represents the linear equation of the line through points
        (x1, y1) and (x2, y2) in slope-intercept (y = mx + b) form, e.g. "y = 3x + 1.5".
-
-        When generating the m value (slope), here are examples of "printable" slopes:
-           5, -5, 1/2, 6/8 (reducing not required), 8/5, -2/3, -18/7
-
-        Here are non-examples of "printable" slopes:
-     1/-2 (should be -1/2), -4/-3 (should be 4/3), 8/4 (should be reduced to 2),
-           -12/3 (should be -4), 3/3 (should be 1), -6/6 (should be -1)
-
-        HINT: Be sure to check if the line is horizontal and return an appropriate string,
-        e.g. y = 6
-        HINT: Absolute value might be helpful for ensuring proper placement of negative sign!
-
-
-        When generating the b value, here are some examples of "printable" y-intercepts:
-           + 1.0 	- 2.35	      + 12.5		- 8.0		+ 17.19
-
-        Here are non-examples of "printable" y-intercepts:
-           - -1.0 	+ -2.35	- -12.5	+ -8.0	     - -17.19	+ 0	- 0
-
-        HINT: Absolute value might be helpful for printing negative y-intercepts as
-               subtraction!
      */
     public String equation() {
         return "y = " + slope() + "x " + yIntercept();
@@ -67,7 +51,10 @@ public class LinearEquation {
     /* Returns a String of the coordinate point on the line that has the given x value, with
        both x and y coordinates as decimals to the nearest hundredth, e.g (-5.0, 6.75) */
     public String coordinateForX(double xValue) {
-
+        xValue = roundedToHundredth(xValue);
+        double yValue = xValue * slope() + yIntercept();
+        yValue = roundedToHundredth(yValue);
+        return "The point on the line is (" + xValue + ", " + yValue + ")";
     }
 
     /* "Helper" method for use elsewhere in your methods; returns the value toRound rounded
@@ -76,7 +63,9 @@ public class LinearEquation {
         HINT:  the Math.round method can help with this!
      */
     public double roundedToHundredth(double toRound) {
-
+        toRound = toRound * 100;
+        toRound = Math.round(toRound)/100.00;
+        return toRound;
     }
 
     /* Returns a string that includes all information about the linear equation, each on
@@ -92,7 +81,10 @@ public class LinearEquation {
 
       */
     public String lineInfo() {
-
+        return "The two points are: (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ")" +
+                "\nThe slope of this line is: " + slope() + "\nThe y-intercept of the line " +
+                "is: " + yIntercept() + "\nThe distance between the two points is: "
+                + distance();
     }
 
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -3,14 +3,27 @@ public class LinearEquationRunner {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome!");
+
         System.out.print("Enter coordinate 1: ");
         String coordinate1 = scan.nextLine();
-        int x1 = Integer.parseInt(coordinate1.substring(1, 2));
-        int y1 = Integer.parseInt(coordinate1.substring(4, 5));
+        int x1 = Integer.parseInt(coordinate1.substring(1, coordinate1.indexOf(",")));
+        int y1 = Integer.parseInt(coordinate1.substring(coordinate1.indexOf(" ") + 1,
+                coordinate1.indexOf(")")));
+
         System.out.print("Enter coordinate 2: ");
         String coordinate2 = scan.nextLine();
-        int x2 = Integer.parseInt(coordinate2.substring(1, 2));
-        int y2 = Integer.parseInt(coordinate2.substring(4, 5));
+        int x2 = Integer.parseInt(coordinate2.substring(1, coordinate2.indexOf(",")));
+        int y2 = Integer.parseInt(coordinate2.substring(coordinate2.indexOf(" ") + 1,
+                coordinate2.indexOf(")")));
+
+        LinearEquation line = new LinearEquation(x1, y1, x2, y2);
+        System.out.println();
+        System.out.println(line.lineInfo());
+        System.out.println();
+        System.out.print("Enter a value for x: ");
+        double xValue = scan.nextDouble();
+        System.out.println();
+        System.out.println("The point on the line is: " + line.coordinateForX(xValue));
 
     }
 }
